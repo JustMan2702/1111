0de2a4f7b464a03ede36cb89bcf45a2ff28bbdb5
Student <student@mirea.ru>
2020-10-24 14:44:19 +0300

    A+B

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
+import java.util.*;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello,Novikov Mikhail");
+       Scanner in = new Scanner( System .in);
+       int a,b;
+       a= in.nextInt();
+       b=in.nextInt();
+       int c = a + b;
+       System.out.println(c);
+
     }
 }
